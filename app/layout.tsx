61d3f19e9d97857ca05eebe516a1467e3e4b9796
Elijah Kurien <elijahkurien05@gmail.com>
2025-07-10 21:15:49 -0400

    OG added

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,21 @@ const dmSans = DM_Sans({
 export const metadata: Metadata = {
   title: "Elijah Kurien",
   description: "My Portfolio",
+  openGraph: {
+    title: "Elijah Kurien",
+    description: "My Personal Website",
+    url: "https://elijahkurien.com", 
+    siteName: "Elijah Kurien",
+    images: [
+      {
+        url: "/preview.png", // Path to your OG image in the public folder
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
