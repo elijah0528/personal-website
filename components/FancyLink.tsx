06d8f70a46f3797169210ec37b2ca6d3754a7942
Elijah Kurien <elijahkurien05@gmail.com>
2026-01-22 11:28:29 -0500

    whitespace-nowrap

--- components/FancyLink.tsx
+++ components/FancyLink.tsx
@@ -14,7 +14,7 @@ export default function FancyLink({
     <a
       href={href}
       className={`
-      relative transition-all duration-300 ease-in-out hover:translate-x-0.5 hover:text-accent-foreground
+      relative whitespace-nowrap transition-all duration-300 ease-in-out hover:translate-x-0.5 hover:text-accent-foreground
       after:content-[''] after:absolute after:h-px after:-bottom-0.5 after:left-0 
       after:bg-current after:transition-all after:duration-300 after:ease-in-out
       after:w-full after:opacity-30 md:after:w-0 md:after:opacity-100 hover:after:w-full hover:after:opacity-100
